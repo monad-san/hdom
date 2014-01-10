5327a69cf2a7827d1e895771699c68ae10c8b523
Yamada Satoshi <mount.sa@gmail.com>
2014-01-10 17:00:39 +0900

    add prototype Console.hs

---
+++ src/Hdom/Console.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE DeriveFunctor #-}
+module Hdom.Console where
+
+import qualified Data.IntMap as IM
+import Control.Monad.Free
+
+data ConsoleF c = Notice String c
+                | Select String (IM.IntMap String -> c)
+                | TurnStart (Player -> c)
+                | GameEnd (String,[(Str,Int)])
+                deriving Functor
+
+type Console = Free ConsoleF
+
+runStdConsole :: Console a -> IO (Console a)
+runStdConsole (Pure a) = return (Pure a)
+runStdConsole (Free (Notice s f)) = undefined
+runStdConsole (Free (Select s co)) = undefined
+runStdConsole (Free (TurnStart co)) = undefined
+runStdConsole (Free (GameEnd (s,score))) = undefined
+
