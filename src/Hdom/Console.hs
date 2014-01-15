6af30a86f34f5af1284251c1389c86f184ed0bb7
Yamada Satoshi <mount.sa@gmail.com>
2014-01-15 15:06:33 +0900

    defined types of operative functions

--- src/Hdom/Console.hs
+++ src/Hdom/Console.hs
@@ -1,15 +1,18 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TemplateHaskell, DeriveFunctor #-}
 module Hdom.Console where
 
 import Control.Monad(join)
 import Data.Maybe(catMaybes)
 import Data.Traversable(sequenceA)
 import qualified Data.IntMap as IM
-import Control.Lens
+import Control.Lens((^.))
 import Control.Monad.Free.Church
-import Hdom.Types
+
+import Hdom.PlayerIO
+
 
 data ConsoleF a = Notice [Int] String a
+                | NoticeAll String a
                 | Select [Int] String (IM.IntMap String -> a)
                 | LiftIO (IO a)
                 | GameEnd (String,[(String,Int)])
@@ -17,14 +20,25 @@ data ConsoleF a = Notice [Int] String a
 
 type Console = F ConsoleF
 
+notice :: [Int] -> String -> Console ()
 notice ns s = wrap $ Notice ns s $ return ()
+
+noticeAll :: String -> Console ()
+noticeAll s = wrap $ NoticeAll s $ return () 
+
+select ::  [Int] -> String -> Console (IM.IntMap String)
 select ns s = wrap $ Select ns s return
+
+liftIO :: IO a -> Console a
 liftIO m = wrap.LiftIO $ fmap return m
 
-runConsole :: IM.IntMap Player -> Console a -> IO a
+runConsole :: IM.IntMap PlayerIO -> Console a -> IO a
 runConsole ps m_ = runF m_ return d where
   d (Notice ns s co) = do
     comb ns $ \p -> p^.pout $ s
+    co
+  d (NoticeAll s co) = do
+    comb (IM.keys ps) $ \p -> p^.pout $ s
     co
   d (Select ns s co) = do
     ss <- comb ns $ \p -> (p^.pout $ s)>>(p^.pin)
@@ -32,10 +46,10 @@ runConsole ps m_ = runF m_ return d where
   d (LiftIO m) = join m
   d (GameEnd (s,score)) = undefined
   
-  comb :: [Int] -> (Player -> IO a) -> IO (IM.IntMap a)
+  comb :: [Int] -> (PlayerIO -> IO a) -> IO (IM.IntMap a)
   comb ns f = sequenceA.(fmap f) $ sps ns
   
-  sps :: [Int] -> IM.IntMap Player
+  sps :: [Int] -> IM.IntMap PlayerIO
   sps ns = IM.fromList $ catMaybes
            $ map (\n -> (IM.lookup n ps)>>=(\p -> return (n,p))) ns
 
