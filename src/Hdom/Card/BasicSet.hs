fc093969d6b03a9396866f21047976abcedf9cea
Yamada Satoshi <mount.sa@gmail.com>
2014-01-16 19:09:07 +0900

    add a part of Bacic set

---
+++ src/Hdom/Card/BasicSet.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE TemplateHaskell, OverloadedStrings #-}
+module Hdom.Card.BasicSet where
+
+import Control.Monad.State
+import Control.Lens
+import Data.Default
+
+import Hdom.Types
+import Hdom.Card
+
+
+market   = kingdom & costs .~ 5
+                   & actionCard ?~ (ActionCard { _effect = market' })
+  where
+    market' = do
+      my.turn.actions += 1
+      my.turn.coins += 1
+      my.turn.buys += 1
+      uses (my.turn) (lift.(execStateT draw))
+
+village  = kingdom & costs .~ 3
+                   & actionCard ?~ (ActionCard { _effect = village' })
+  where
+    village' = do
+      my.turn.actions += 2
+      uses (my.turn) (lift.(execStateT draw))
+
+festival = kingdom & costs .~ 5
+                   & actionCard ?~ (ActionCard { _effect = festival' })
+  where
+    festival' = do
+      my.turn.actions += 2
+      my.turn.coins += 2
+      my.turn.buys += 1
+
+
+basicSet :: CardInfo
+basicSet = M.fromList [ ("Market", market)
+                      , ("Village", village)
+                      , ("Festival", festival)]
+
